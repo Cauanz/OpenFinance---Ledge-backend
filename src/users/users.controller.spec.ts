29c577104f5eb6f03c74bc498a10eb3a62464e6b
Cauanz <61758981+Cauanz@users.noreply.github.com>
2026-09-08 19:20:55 -0300

    feat: add vitest dependency and update UsersController tests
    - Added 'vitest' as a dependency in package.json.
    - Refactored UsersController tests to use node:test framework.
    - Initialized UsersService in tests and added a test case for getProfile method.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,19 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { beforeEach, describe } from 'node:test';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
 
 describe('UsersController', () => {
-  let controller: UsersController;
-  //TODO - ESCREVER TESTES
+  let usersController: UsersController;
+  let usersService: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
-
-    controller = module.get<UsersController>(UsersController);
+  beforeEach(() => {
+    usersService = new UsersService();
+    usersController = new UsersController(usersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('register', () => {
+    it('Should get user profile and return it', async () => {});
+
+    //TODO - CONTINUAR ESCREVENDO ESSES TESTES
+
+    const result: User = {
+      id: 2,
+      username: 'cauanzela',
+      email: 'valid.email@gmail.com',
+      password: '$2b$10$sagPg9vIx54msAsXDG7zUu9L2FYYb6oX9.s25rKH7qs6be3bkNkYi',
+      createdAt: '2026-08-31T21:46:53.000Z',
+      updatedAt: '2026-08-31T21:46:53.000Z',
+    };
+
+    expect(await usersController.getProfile()).toBe(result);
   });
 });
