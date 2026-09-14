f3038fb9f5b9bfc5d2d2547b40693b9a60518200
Cauanz <61758981+Cauanz@users.noreply.github.com>
2026-09-14 19:10:05 -0300

    feat: Rename the login endpoint to signin and remove UsersService from AuthModule

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,7 +20,7 @@ export class AuthController {
   }
 
   @HttpCode(HttpStatus.OK)
-  @Post('login')
+  @Post('signin')
   signIn(@Body() signInData: Record<string, any>) {
     return this.authService.signIn(signInData.email, signInData.password);
   }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
+// import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UsersService],
+  providers: [AuthService],
   imports: [
     UsersModule,
     JwtModule.register({
